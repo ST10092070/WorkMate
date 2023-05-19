06dcfa1964e102482866ca9f2bbd8e15f34b7b00
corne-ac <corne.ackerman1104@gmail.com>
2023-05-19 11:50:29 +0200

    Implemented Navigation on activity_login.xml TODO: Implement validation&registration

--- app/src/main/java/com/opsc/workmate/ui/LoginFragment.kt
+++ app/src/main/java/com/opsc/workmate/ui/LoginFragment.kt
@@ -1,11 +1,18 @@
 package com.opsc.workmate.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.opsc.workmate.MainActivity
 import com.opsc.workmate.R
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +42,48 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        //---- Navigate to register screen ----
+        // Find the TextView by ID
+        val txtSignUp: TextView = view.findViewById(R.id.txtSignUp)
+
+        // Set onClickListener for the TextView
+        txtSignUp.setOnClickListener {
+            // Get the NavController
+            val navController = Navigation.findNavController(view)
+
+            // Navigate to the registerFragment
+            navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        //---- Login button ----
+        //Find the button by ID
+        val btnLogin: Button = view.findViewById(R.id.btnLogin)
+
+        // Set onClickListener for button
+        btnLogin.setOnClickListener {
+            // Perform login validation
+            val isValid = validateLogin()
+
+            if (isValid) {
+                // Navigate to MainActivity
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Display an error message or handle the invalid login case
+                Toast.makeText(activity, "Invalid login credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        return view
+    }
+
+    private fun validateLogin(): Boolean {
+        //TODO: Implement validation for login
+        //Use this method, return true for successful, return false for fail
+        return true
     }
 
     companion object {
--- app/src/main/java/com/opsc/workmate/ui/RegisterFragment.kt
+++ app/src/main/java/com/opsc/workmate/ui/RegisterFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.opsc.workmate.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -35,7 +39,47 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        //---- Navigate to register screen ----
+        // Find the TextView by ID
+        val txtLogIn: TextView = view.findViewById(R.id.txtLogIn)
+
+        // Set onClickListener for the TextView
+        txtLogIn.setOnClickListener {
+            // Get the NavController
+            val navController = Navigation.findNavController(view)
+
+            // Navigate to the registerFragment using the action defined in the nav_graph_login.xml
+            navController.navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        //---- Register Button ----
+        // Find the Button by ID
+        val btnRegister: Button = view.findViewById(R.id.btnRegister)
+
+        // Set OnClickListener for the Button
+        btnRegister.setOnClickListener {
+            // Perform input validation
+            val isValid = validateInput()
+
+            if (isValid) {
+                // Get the NavController
+                val navController = Navigation.findNavController(view)
+                // Navigate to the registerFragment
+                navController.navigate(R.id.action_registerFragment_to_loginFragment)
+            } else {
+                // Display an error message or handle the invalid input case
+                Toast.makeText(activity, "Invalid input", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
+    }
+
+    private fun validateInput(): Boolean {
+        //TODO: Implement registration
+        return true
     }
 
     companion object {
